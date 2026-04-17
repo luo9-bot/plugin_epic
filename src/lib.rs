2c6821fa944c4db746476b6cc796aa536f071904
drluo <2557657882@qq.com>
2026-04-17 13:33:11 +0800

    build: 更新依赖版本并重构消息构建方式
    - 将 luo9_sdk 从 0.2.0 升级至 0.3.0
    - 将 tokio 从 1.51.1 升级至 1.52.1
    - 使用新的 Msg 构建器 API 重构群消息发送逻辑，提升可读性与类型安全

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod core;
 pub mod epic;
 
 use luo9_sdk::Bot;
+use luo9_sdk::Msg;
 use luo9_sdk::command::Command;
 use luo9_sdk::command::PrefixMode;
 
@@ -21,16 +22,16 @@ pub fn handle_group_msg(group_id: u64, _user_id: u64, msg: &str) {
             
             for game in free_games {
                 game_str.push_str(&format!("{:?}\n", game));
-                msg = CString::new(format!(
-                    "游戏名称: {}\n描述: {}\n开发商/发行商: {}\n免费结束时间: {}\n商店链接: {}\n预览图片: [CQ:image,url={}]", 
-                    game.title, 
-                    game.description, 
-                    game.seller, 
-                    game.free_end_date, 
-                    game.store_url, 
-                    game.preview_image).as_str()).unwrap();
+                msg = Msg::at(_user_id).endl()
+                .txt(format!("游戏名称: {}", game.title)).endl()
+                .txt(format!("描述: {}", game.description)).endl()
+                .txt(format!("开发商/发行商: {}", game.seller)).endl()
+                .txt(format!("免费结束时间: {}", game.free_end_date)).endl()
+                .txt(format!("商店链接: {}", game.store_url)).endl()
+                .txt("预览图片：").image(game.preview_image)
+                .build();
 
-                    Bot::send_group_msg(group_id, msg);
+                Bot::send_group_msg(group_id, msg);
             }
 
         }
